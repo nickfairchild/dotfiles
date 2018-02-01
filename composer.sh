4b0022a5292c04352d182ba94618cfe7d86100ab
Nick Fairchild <nick.fairchild@gmail.com>
2018-02-01 17:31:27 +0000

    composer script

---
+++ composer.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Install valet
+composer global require "laravel/valet"
+
+# Laravel installer
+composer global require "laravel/installer"
+
+# Spark installer
+composer global require "laravel/spark-installer"
+
+spark register KjANeZRBXDlvv72XUVp3izkrBcIXsUONQPYbcy0eraPNcPiPxwYjh1Huvdib
